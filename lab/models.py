3dcadf01e4bf72681a360b184ef552173c3d166c
Michael McAuliffe <michael.e.mcauliffe@gmail.com>
2013-01-29 11:46:03 -0800

    Initial.

--- lab/models.py
+++ lab/models.py
@@ -4,38 +4,37 @@ from mezzanine.pages.models import Page
 from .helper import generate_file_name
 
 # Create your models here.
+    
 
-
-class Person(models.Model):
+class LabMember(models.Model):
     first_name = models.CharField(max_length=250)
     last_name = models.CharField(max_length=250)
     
-    class Meta:
-        abstract = True
-    
-    def __unicode__(self):
-        return u'%s %s' % (self.first_name,self.last_name)
-    
-
-class LabMember(Person):
-    
-    research_interests = models.TextField()
-    email = models.EmailField()
-    picture = models.ImageField(upload_to='people')
+    research_interests = models.TextField(null=True,blank=True)
+    email = models.EmailField(null=True,blank=True)
+    picture = models.ImageField(upload_to='people',null=True,blank=True)
     position = models.ForeignKey('Position')
     
     class Meta:
         ordering = ['position__importance','last_name','first_name']
     
-class Collaborator(Person):
-    link = models.URLField(max_length=250)
+    def __unicode__(self):
+        return u'%s %s' % (self.first_name,self.last_name)
+    
+class Collaborator(models.Model):
+    first_name = models.CharField(max_length=250)
+    last_name = models.CharField(max_length=250)
+    link = models.URLField(max_length=250,null=True,blank=True)
     
     class Meta:
         ordering = ['last_name','first_name']
     
+    def __unicode__(self):
+        return u'%s %s' % (self.first_name,self.last_name)
+    
 class WrittenByLab(models.Model):
     person = models.ForeignKey(LabMember)
-    publication = models.ForeignKey('ResearchResult')
+    publication = models.ForeignKey('Publication')
     author_number = models.IntegerField()
     
     class Meta:
@@ -43,18 +42,46 @@ class WrittenByLab(models.Model):
     
 class WrittenByCollab(models.Model):
     person = models.ForeignKey(Collaborator)
-    publication = models.ForeignKey('ResearchResult')
+    publication = models.ForeignKey('Publication')
     author_number = models.IntegerField()
     
     class Meta:
         ordering = ['author_number']
-        
-class ResearchResult(models.Model):
-    lab_author = models.ManyToManyField(LabMember,through='WrittenByLab', related_name="%(app_label)s_%(class)s_related")
-    collaborator_author = models.ManyToManyField(Collaborator,through='WrittenByCollab', related_name="%(app_label)s_%(class)s_related")
+    
+class PresentedByLab(models.Model):
+    person = models.ForeignKey(LabMember)
+    publication = models.ForeignKey('Presentation')
+    author_number = models.IntegerField()
+    
+    class Meta:
+        ordering = ['author_number']
+    
+class PresentedByCollab(models.Model):
+    person = models.ForeignKey(Collaborator)
+    publication = models.ForeignKey('Presentation')
+    author_number = models.IntegerField()
+    
+    class Meta:
+        ordering = ['author_number']
+    
+    
+class Publication(models.Model):
+    STATE_CHOICES = (('published','Published'),
+                    ('in press','In press'),
+                    ('under review','Under review'),
+                    ('under revision','Under revision'),
+                    )
+    lab_author = models.ManyToManyField(LabMember,through='WrittenByLab')
+    collaborator_author = models.ManyToManyField(Collaborator,through='WrittenByCollab')
     year = models.IntegerField()
     title = models.CharField(max_length=500)
-    pdf = models.FileField(upload_to = generate_file_name)
+    pdf = models.FileField(upload_to = generate_file_name,null=True,blank=True)
+    
+    journal = models.CharField(max_length=250)
+    volume = models.IntegerField()
+    number = models.IntegerField()
+    pages = models.CharField(max_length=100)
+    state = models.CharField(max_length=50,choices=STATE_CHOICES)
     
     class Meta:
         abstract = True
@@ -82,13 +109,6 @@ class ResearchResult(models.Model):
     def generate_key(self):
         key = '%s%d%s' % (self.get_authors()[0].last_name,self.year,self.title.split(" ")[0])
         return key
-    
-    
-class Publication(ResearchResult):
-    journal = models.CharField(max_length=250)
-    volume = models.IntegerField()
-    number = models.IntegerField()
-    pages = models.CharField(max_length=100)
         
     def get_bibtext(self):
         tex = """@article{%s,
@@ -105,9 +125,42 @@ class Publication(ResearchResult):
         return tex
     
         
-class Presentation(ResearchResult):
+class Presentation(models.Model):
+    lab_author = models.ManyToManyField(LabMember,through='PresentedByLab')
+    collaborator_author = models.ManyToManyField(Collaborator,through='PresentedByCollab')
+    year = models.IntegerField()
+    title = models.CharField(max_length=500)
+    pdf = models.FileField(upload_to = generate_file_name,null=True,blank=True)
+    
     conference = models.CharField(max_length = 500)
     location = models.CharField(max_length = 250)
+    
+    class Meta:
+        abstract = True
+        ordering = ['-year','title']
+    
+    def get_authors(self):
+        labs = list(self.lab_authorset.all())
+        collabs = list(self.collaborator_authorset.all())
+        author_list = []
+        for i in range(1,len(labs)+len(collabs)+1):
+            if labs[0].author_number < collabs[0].author_number:
+                author_list.append(labs.pop(0))
+            else:
+                author_list.append(collabs.pop(0))
+        return author_list
+        
+    def get_author_string(self):
+        authors = self.get_authors()
+        if len(authors) == 0:
+            return '%s, %s.' % (authors[0].last_name,authors[0].first_name[0])
+        author_string = '%s and %%s' % ('%s, ' * (len(authors)-1))
+        author_string = author_string % tuple('%s, %s.' % (x.last_name,x.first_name[0]) for x in authors)
+        return author_string
+        
+    def generate_key(self):
+        key = '%s%d%s' % (self.get_authors()[0].last_name,self.year,self.title.split(" ")[0])
+        return key
     
     
 class Position(models.Model):
