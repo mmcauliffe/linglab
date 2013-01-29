fb3988f9b96dedc4c2aaed7ef58fc84ed88a4a43
Michael McAuliffe <michael.e.mcauliffe@gmail.com>
2013-01-29 13:23:28 -0800

    Initial.

--- lab/views.py
+++ lab/views.py
@@ -1,23 +1,24 @@
 # Create your views here.
 from django.shortcuts import render,redirect,render_to_response
+from django.template import RequestContext
 
 from .models import Publication,LabMember,Presentation
 
 def publications(request):
     pubs = Publication.objects.all()
     #pub_format = '%s (%s). %s'
-    return render_to_response('lab/publications.html',{'pubs':pubs})
+    return render_to_response('lab/publications.html',{'pubs':pubs},context_instance=RequestContext(request))
 
 def people(request):
     members = LabMember.objects.all()
     #pub_format = '%s (%s). %s'
-    return render_to_response('lab/people.html',{'members':members})
+    return render_to_response('lab/people.html',{'members':members},context_instance=RequestContext(request))
 
 def presentations(request):
     presents = Presentation.objects.all()
     #pub_format = '%s (%s). %s'
-    return render_to_response('lab/presentations.html',{'presents':presents})
+    return render_to_response('lab/presentations.html',{'presents':presents},context_instance=RequestContext(request))
     
 def member(request,member_id):
     person = LabMember.objects.get(pk = member_id)
-    return render_to_response('lab/member.html',{'person':person})
+    return render_to_response('lab/member.html',{'person':person},context_instance=RequestContext(request))
