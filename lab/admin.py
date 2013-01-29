4221ca418fe61a306107d34e7b2a251c00f14808
Michael McAuliffe <michael.e.mcauliffe@gmail.com>
2013-01-29 14:16:24 -0800

    Initial.

--- lab/admin.py
+++ lab/admin.py
@@ -1,6 +1,18 @@
 from django.contrib import admin
 from mezzanine.pages.admin import PageAdmin
-from .models import LabMember,Collaborator,Publication,Presentation,Position
+from .models import *
+
+class LabAuthorshipInline(admin.TabularInline):
+    model = WrittenByLab
+    
+class CollabAuthorshipInline(admin.TabularInline):
+    model = WrittenByCollab
+
+class LabPresentershipInline(admin.TabularInline):
+    model = PresentedByLab
+    
+class CollabPresentershipInline(admin.TabularInline):
+    model = PresentedByCollab
 
 class LabMemberAdmin(admin.ModelAdmin):
     list_display = ('first_name','last_name','email')
@@ -14,11 +26,15 @@ admin.site.register(Collaborator,CollaboratorAdmin)
 
 class PublicationAdmin(admin.ModelAdmin):
     list_display = ('generate_key',)
+    inlines = [LabAuthorshipInline,
+                CollabAuthorshipInline]
     
 admin.site.register(Publication,PublicationAdmin)
 
 class PresentationAdmin(admin.ModelAdmin):
     list_display = ('generate_key',)
+    inlines = [LabPresentershipInline,
+                CollabPresentershipInline]
     
 admin.site.register(Presentation,PresentationAdmin)
 
